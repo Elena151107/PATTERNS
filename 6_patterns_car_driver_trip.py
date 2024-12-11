04539407ae269128040e4191d945c9a1a2643f2e
Elena151107 <162087911+Elena151107@users.noreply.github.com>
2024-12-11 22:28:20 +0700

    Update 6_patterns_car_driver_trip.py

--- 6_patterns_car_driver_trip.py
+++ 6_patterns_car_driver_trip.py
@@ -22,7 +22,6 @@
 - Заитите доступ к данным водителей с помоью прокси."""
 
 from abc import ABC, abstractmethod
-from typing import Dict,List
 
 class Driver():
     def __init__(self, name: str=None, rating: float=0, bonuses: int=0):
@@ -62,6 +61,7 @@ class CarType(CarComponent):
 class Car(CarComponent):
     def __init__(self, car_type: CarType):
         self.car_type = car_type
+        self.car_brand = self.car_type.car_brand
 
     def info_by_car(self, indent=0):
         return self.car_type.info_by_car()
@@ -87,20 +87,20 @@ class CarFactory():
 
 #composite
 class CarComposite(CarComponent):
-    def __init__(self, name: str):
+    def __init__(self, name: str=None):
         self.name = name
         self._cars = []
 
-    def add_car(self, car: CarComponent):
+    def add_car(self, car):
         self._cars.append(car)
 
-    def remove(self, car: CarComponent):
+    def remove(self, car):
         self._cars.remove(car)
 
     def info_by_car(self, indent=0):
         print(' '*(indent+4) + f'{self.name}:')
         for num, car in enumerate(self._cars, 1):
-            print(" "*(indent+8) + f'{num}: {car.info_by_car()}')
+            print(" " * (indent + 8) + f'{num}: {car.info_by_car()}')
 
 
 class GPS(ABC):
@@ -112,6 +112,8 @@ class GPS(ABC):
 class GPSAdapter(GPS):
     def __init__(self, adapt: Car):
         self.adapt = adapt
+        self.location = None
+        self.car_brand = None
 
     def get_location(self, location: str='База'):
         return self.adapt.set_loc(location)
@@ -131,83 +133,109 @@ class PremiumDecorator(GPSAdapter):
     def __init__(self, car: GPSAdapter, premium_paket: str='Включен премиум-сервис: улучшенное качество обслуживания, прохладительный приветственный напиток, Wi-Fi'):
         self.car = car
         self.premium_paket = premium_paket
+        self.location = None
+        self.car_brand = None
 
     def info_by_car(self, indent=0):
-        return f'{self.car.info_by_car()}\n\t\t\t<<{self.premium_paket}>>'
+        return f'{self.car.info_by_car()}\n\t\t\t<< {self.premium_paket} >>'
 
     def all_info(self):
-        return f'{self.car.all_info()}\n\t\t\t<<{self.premium_paket}>>'
+        return f'{self.car.all_info()}\n\t\t<< {self.premium_paket} >>'
 
 
 class Trip():
-    def __init__(self, start_location: str=None, end_location: str=None, premium_paket: bool=False, vehicle=None, driver=None):
+    def __init__(self, start_location: str=None, end_location: str=None, premium_paket: bool=False):
         self.start_location = start_location
         self.end_location = end_location
         self.premium_paket = premium_paket
         self.status = 'Поиск машины'
-        self.vehicle = vehicle
-        self.driver = driver
 
     def __str__(self):
-        return f'Заказ оформлен:\nПоездка от места: {self.start_location} до места: {self.end_location}. {"Премиум-сервис включен" if self.premium_paket else ""}\nStatus: {self.status}'
+        return f'Заказ оформлен:\nПоездка от места: {self.start_location}, до места: {self.end_location}. {"Премиум-сервис включен" if self.premium_paket else ""}\nStatus: {self.status}'
 
-    def add_car(self, vehicle: GPSAdapter):
-        if vehicle is None:
+    def add_car(self, car):
+        if car is None:
             print('Идет поиск машины')
         else:
-            return f'Заказ подтвержден:\nПоездка от места: {self.start_location} до места: {self.end_location}. \nStatus: Назначен {vehicle.all_info()}'
+            return f'Status: Назначен {car.all_info()}\nПоездка от: {self.start_location}\n\t\t до: {self.end_location}'
+  
+    def set_driver(self, driver: Driver, car):
+        if isinstance(car, GPSAdapter):
+            driver.bonuses = 500
+        if isinstance(car, PremiumDecorator):
+            driver.bonuses = 1000
+        return driver.info_by_driver()
 
-    def add_car_with_premium(self, vehicle: PremiumDecorator):
-        if vehicle is None:
-            print('Идет поиск машины')
-        else:
-            return f'Заказ подтвержден:\nПоездка от места: {self.start_location} до места: {self.end_location}. \nStatus: Назначен {vehicle.all_info()}'
 
 #proxy
 class TripProxy(Trip):
     def __init__(self, trip: Trip):
         self.trip = trip
+        self.start_location = self.trip.start_location
+        self.end_location = self.trip.end_location
+        self.premium_paket = self.trip.premium_paket
+        self.status = self.trip.status
 
-    def get_driver(self, vehicle: GPSAdapter, driver: Driver):
-        if not self.trip.add_car(vehicle):
-                return 'Водитель не назначен'
-        else:
-            driver.bonuses += 500
-            return f'{self.trip.add_car(vehicle)}\n{vehicle.set_driver(driver)}'
+        self.car_type = CarType()
+        self.car = Car(self.car_type)
 
-    def get_driver_premium(self, vehicle: PremiumDecorator, driver: Driver):
-        if not self.trip.add_car_with_premium(vehicle):
+    def get_driver(self, driver, car):
+        if driver is None:
             return 'Водитель не назначен'
         else:
-            driver.bonuses += 1000
-            return f'{self.trip.add_car_with_premium(vehicle)}\n{vehicle.set_driver(driver)}'
+            return f'{self.trip.add_car(car)}\n{self.trip.set_driver(driver, car)}'
 
 
 #facade
 class Facade():
     def __init__(self):
-        self.trip = TripProxy(Trip())
         self._trips = {}
+        self._orders = []
 
-     def add_trip(self, trip: TripProxy):
-        if trip in self._trips:
-            print(f'ВНИМАНИЕ: Такой заказ уже был создан и подтвержден!\n')
-        else:
+    def add_trip(self, trip: TripProxy):
+        if trip not in self._trips:
             self._trips[trip] = TripProxy(Trip())
 
-    def get_status_orders(self):
+    def get_status_trips(self):
         self.get_cars_in_park()
-        print('\n***** ORDERS: *****')
+        print('<<<  Заказы: >>>')
         for num, i in enumerate(self._trips, 1):
             print(f'{num}.\n{i}')
 
+    def get_orders(self, trip, driver, car):
+        if trip not in self._orders:
+            self._orders.append(trip.get_driver(driver, car))
+        # if trip.car.car_brand in self._orders:
+        #     for i in self._orders:
+        #         trip.car[i].location = trip.car[i-1].end_location
+
+    def show_orders(self):
+        print('_____________________________________________')
+        print('           ***  ORDERS: ***')
+        for num, i in enumerate(self._orders, 1):
+            print(f'--- Trip: {num}. ---\n{i}\n')
+
     def get_cars_in_park(self):
-        print('ПАРК АВТОТРАНСПОРТА:')
+        print('ВЕСЬ ПАРК АВТОТРАНСПОРТА:')
         print('______________________')
         for num, i in enumerate(CarFactory.get_cars_in_park(), 1):
             print(f'{num}. {i}')
-        print('______________________')
+        print('_____________________________')
+        print('_____________________________')
 
+    def add_car_composite(self, composite, *args):
+        for arg in args:
+            composite.add_car(arg)
+
+    def remove_composite(self, composite, car):
+        composite.remove(car)
+
+    def info_by_composite(self, *args):
+        print('Тип транспортных средств:')
+        print('______________________')
+        for arg in args:
+            arg.info_by_car()
+        print('______________________')
 
 
 driver_1 = Driver('John', 5.0)
@@ -240,46 +268,34 @@ type1 = CarComposite('Все легковые автомобили')
 type2 = CarComposite('Все минивэны')
 type3 = CarComposite('Все электромобили')
 
-print('ПАРК ТРАНСПОРТНЫХ СРЕДСТВ:')
-type1.add_car(car3)
-type1.add_car(decorator_mercedes)
-type1.info_by_car()
-
-type2.add_car(decorator_prado)
-type2.add_car(car2)
-type2.info_by_car()
-
-type3.add_car(car5)
-type3.add_car(decorator_chevr_bolt)
-type3.add_car(car7)
-type3.info_by_car()
 print()
 
-print('<<<  Заказы на оформлении: >>>')
-print('________________________________')
-print('*** TRIP 1 ***')
 trip1 = Trip('ul.Dostoevskogo, 15', 'ul. Lenina, 25')
-print(trip1)
-print()
-proxy1 = TripProxy(trip1)
-proxy1 = proxy1.get_driver(gps3,driver_1)
-
-print('*** TRIP 2 ***')
 trip2 = Trip('pl.Lenina', 'pl.Red Prospect', premium_paket=True)
-print(trip2)
-print()
-proxy2 = TripProxy(trip2)
-proxy2 = proxy2.get_driver_premium(decorator_mercedes, driver_2)
-
-print('*** TRIP 3 ***')
 trip3 = Trip('ul.Distinkt, 23', 'ul. Minina, 99')
-print(trip3)
-print()
+trip3 = Trip('ul.Distinkt, 23', 'ul. Minina, 99')
+
+proxy1 = TripProxy(trip1)
+proxy2 = TripProxy(trip2)
 proxy3 = TripProxy(trip3)
-proxy3 = proxy3.get_driver_premium(decorator_mercedes,driver_1)
+proxy3 = TripProxy(trip3)
 
 facade = Facade()
+
+facade.add_car_composite(type1, car3, decorator_mercedes)
+facade.add_car_composite(type2, decorator_prado, car2)
+facade.add_car_composite(type3, car5, decorator_chevr_bolt, car7)
+facade.info_by_composite(type1, type2, type3)
+print()
+
 facade.add_trip(proxy1)
 facade.add_trip(proxy2)
 facade.add_trip(proxy3)
-facade.get_status_orders()
+facade.add_trip(proxy3)
+facade.get_status_trips()
+print()
+
+facade.get_orders(proxy1, driver_1, decorator_mercedes)
+facade.get_orders(proxy2, driver_2, gps3)
+facade.get_orders(proxy3, driver_3, gps3)
+facade.show_orders()
